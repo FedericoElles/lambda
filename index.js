89eb967053777987b5e464cada072754ff524570
Federico Elles <FedericoElles@users.noreply.github.com>
2016-02-29 08:19:43 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var hello_world = require('./hello_world/index.js');
 var libelle_news = require('./libelle_news/index.js');
 
 exports.uuid = hello_world.uuid;
-exports.libelle_news = libelle_news;
+exports.libelle_news = libelle_news.libelle_news;
