13f6599a573870688421b454732840977fbf8691
Federico Elles <FedericoElles@users.noreply.github.com>
2016-03-04 07:56:53 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,9 @@
 var hello_world = require('./hello_world/index.js');
 var libelle_news = require('./libelle_news/index.js');
 var fetch_from_ftp = require('./fetch_from_ftp/index.js');
+var familynavi_temp = require('./familynavi_temp/index.js');
 
 exports.uuid = hello_world.uuid;
 exports.libelle_news = libelle_news.libelle_news;
 exports.fetch_from_ftp = fetch_from_ftp.fetch_from_ftp;
+exports.familynavi_temp = familynavi_temp.familynavi_temp;
